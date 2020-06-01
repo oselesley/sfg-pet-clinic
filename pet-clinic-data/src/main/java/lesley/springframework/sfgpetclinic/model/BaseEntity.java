178306d2ea3ad6f2e907075c5559f1a237f99756
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-06-01 05:21:48 -0700

    adding base entity. closes #3

---
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package lesley.springframework.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isNew () {
+        return this.id == null;
+    }
+}
--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package lesley.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
