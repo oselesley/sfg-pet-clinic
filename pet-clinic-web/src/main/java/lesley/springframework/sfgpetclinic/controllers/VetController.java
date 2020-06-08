1952e1b6dafa2357b72450d695a17fa9fc1e43eb
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-06-08 09:13:53 -0700

    added vet index page and controller. closes #14.

---
+++ pet-clinic-web/src/main/java/lesley/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,24 @@
+package lesley.springframework.sfgpetclinic.controllers;
+
+import lesley.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    private VetService vetServiceMap;
+
+    public VetController(@Qualifier("vetServiceMap") VetService vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
+    @RequestMapping("/vets")
+    public String vetpage(Model model) {
+        model.addAttribute("vets", vetServiceMap.findAll());
+
+        return "vets/index";
+    }
+}
