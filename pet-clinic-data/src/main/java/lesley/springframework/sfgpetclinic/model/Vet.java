fac9f6c6b2b4876b352a4cbbe9e87ec058f1ee65
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-07-05 12:29:31 +0100

    added visits, updated owner with pet relationship. closes #25

--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,13 @@
 package lesley.springframework.sfgpetclinic.model;
 
 public class Vet extends Person {
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
