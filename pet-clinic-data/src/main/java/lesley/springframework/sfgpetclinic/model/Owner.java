35fcd1cb1a045ca0c6ef2d079e47325ce4da06f1
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-07-05 13:13:50 +0100

    added contact properties. closes #26

--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Owner.java
@@ -3,10 +3,33 @@ package lesley.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
-    public Owner(Set<Pet> pets) {
-        this.pets = pets;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
