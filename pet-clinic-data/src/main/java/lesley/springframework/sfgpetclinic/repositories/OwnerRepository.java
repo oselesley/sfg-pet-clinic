41b436cdff4e5ab9589d92baf979f0bde0786250
lesley okoduwa <leslieokoduwa@gmail.com>
2020-07-13 19:42:29 +0100

    added spring data jpa service for owners. closes #38

--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,6 +4,9 @@ import lesley.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Optional<Owner> findOwnerByLastName (String lastname);
 }
---
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/services/jpa/OwnerServiceJpa.java
@@ -0,0 +1,41 @@
+package lesley.springframework.sfgpetclinic.services.jpa;
+
+import lesley.springframework.sfgpetclinic.model.Owner;
+import lesley.springframework.sfgpetclinic.repositories.OwnerRepository;
+import lesley.springframework.sfgpetclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class OwnerServiceJpa implements OwnerService {
+    private OwnerRepository ownerRepository;
+
+    public OwnerServiceJpa(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastname) {
+        return ownerRepository.findOwnerByLastName(lastname).orElse(null);
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        Optional<Owner> owner = ownerRepository.findById(aLong);
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+}
