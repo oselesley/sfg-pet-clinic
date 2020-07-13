a21e4d2ff8d9546019123bce9220f24ad11d1491
lesley okoduwa <leslieokoduwa@gmail.com>
2020-07-13 16:20:27 +0100

    created visit entities. closes #36

--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package lesley.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +21,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
--- pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/lesley/springframework/sfgpetclinic/model/Visit.java
@@ -1,10 +1,16 @@
 package lesley.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
